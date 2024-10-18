261ee33ffb4785dc3832d17aa5fd413b34103c5f
Michael <kinovar13@gmail.com>
2024-10-18 20:08:16 +0700

    first commit

---
+++ src/day6/Task2Motorbike.java
@@ -0,0 +1,25 @@
+package day5;
+
+public class Task2Motorbike {
+    private int year;
+    private String model;
+    private String color;
+
+    public Task2Motorbike(int year, String color, String model) {
+        this.year = year;
+        this.color = color;
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
